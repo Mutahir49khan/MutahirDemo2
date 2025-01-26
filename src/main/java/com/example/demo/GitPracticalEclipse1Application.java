51b0d1a5f2648c7db9384ba2e338797c0bc65e33
Mutahir khan <mutahir49khan@gmail.com>
2025-01-26 11:06:10 +0530

    busmillah ist commit

---
+++ src/main/java/com/example/demo/GitPracticalEclipse1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPracticalEclipse1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPracticalEclipse1Application.class, args);
+	}
+
+}
